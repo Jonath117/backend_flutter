58e4e7ccc68e49ebb6cbd40788595d22ba65cf0c
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-11 10:57:37 -0400

    configuracion para que el programa lea las variable del .env al arrancar

--- expense-track-backend/src/expense-track-backend/Program.cs
+++ expense-track-backend/src/expense-track-backend/Program.cs
@@ -1,4 +1,15 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load();
+
+var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+var dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
+
+var connectionString = $"Host=localhost;Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass}";
 
 // Add services to the container.
 
