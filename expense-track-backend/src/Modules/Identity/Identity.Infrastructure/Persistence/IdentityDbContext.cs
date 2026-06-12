dfa346f8163ddc0d1a7c467e9c8278a9794fdbc0
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-12 18:44:21 -0400

    implementacion del administrador de este esquema de la base de datos

---
+++ expense-track-backend/src/Modules/Identity/Identity.Infrastructure/Persistence/IdentityDbContext.cs
@@ -0,0 +1,20 @@
+using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure.Persistence;
+
+public class IdentityDbContext : DbContext
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+    }
+}
