9cc0ce83021f3e1ee9bb46cb178938adde6faa07
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-12 17:03:03 -0400

    se inyecto cada modulo que manejan sus repositorios y dbcontext internos

--- expense-track-backend/src/expense-track-backend/Program.cs
+++ expense-track-backend/src/expense-track-backend/Program.cs
@@ -1,8 +1,15 @@
+using Categories.Infrastructure;
 using DotNetEnv;
+using expense_track_backend.Persistence;
+using Expenses.Infrastructure;
+using Identity.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Reporting.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Env.Load();
+var envPath = Path.Combine(builder.Environment.ContentRootPath, ".env");
+Env.Load(envPath);
 
 var dbUser = Environment.GetEnvironmentVariable("DB_USER");
 var dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD");
@@ -10,6 +17,15 @@ var dbName = Environment.GetEnvironmentVariable("DB_NAME");
 var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "5432";
 
 var connectionString = $"Host=localhost;Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass}";
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
+builder.Services.AddIdentityInfrastructure(connectionString);
+builder.Services.AddCategoriesInfrastructure(connectionString);
+builder.Services.AddExpensesInfrastructure(connectionString);
+builder.Services.AddReportingInfrastructure(connectionString);
+
 
 // Add services to the container.
 
@@ -25,10 +41,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
