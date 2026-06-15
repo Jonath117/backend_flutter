21546deb7433e03e65c5e592d9d2db9075e5008a
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-15 19:52:36 -0400

    se agregaron los campos Name y Lastname en lugar de solo Username

--- expense-track-backend/src/Modules/Identity/Identity.Domain/Entities/User.cs
+++ expense-track-backend/src/Modules/Identity/Identity.Domain/Entities/User.cs
@@ -4,7 +4,9 @@ public class User
 {
     public Guid Id { get; private set; }
     public String Email { get; private set; } = string.Empty;
-    public String Username { get; private set; } = string.Empty;
+    
+    public String Name { get; private set; } = string.Empty;
+    public String LastName { get; private set; } = string.Empty;
     public String PasswordHash { get; private set; } = string.Empty;
     public DateTime CreatedAt { get; private set; }
     public DateTime? UpdatedAt { get; private set; }
