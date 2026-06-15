dd2050046c2096953973cba5fe2849a5c146c93e
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-15 19:54:10 -0400

    migracion

--- expense-track-backend/src/expense-track-backend/Migrations/20260615234612_UpdateUserNames.Designer.cs
+++ expense-track-backend/src/expense-track-backend/Migrations/20260615234612_UpdateUserNames.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using expense_track_backend.Persistence;
@@ -11,9 +12,11 @@ using expense_track_backend.Persistence;
 namespace expense_track_backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260615234612_UpdateUserNames")]
+    partial class UpdateUserNames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ expense-track-backend/src/expense-track-backend/Migrations/20260615234612_UpdateUserNames.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace expense_track_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "username",
+                schema: "identity",
+                table: "users",
+                newName: "name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                schema: "identity",
+                table: "users",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                schema: "identity",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                schema: "identity",
+                table: "users",
+                newName: "username");
+        }
+    }
+}
