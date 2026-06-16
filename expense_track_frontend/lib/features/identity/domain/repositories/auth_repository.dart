5fa14d00e62d4341f387c037976d9d989fa0ed32
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-16 14:20:41 -0400

    declaracion del contrato para iniciar sesion y registrarse

---
+++ expense_track_frontend/lib/features/identity/domain/repositories/auth_repository.dart
@@ -0,0 +1,10 @@
+abstract class AuthRepository {
+  Future<String> login(String email, String password);
+
+  Future<String> register(
+    String email,
+    String name,
+    String lastName,
+    String password,
+  );
+}
