3d6494e69d64080254ec13e62613f21816aef66d
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-22 02:43:48 -0400

    implementacion del registro de archivos

--- expense-track-backend/src/Modules/Expenses/Expenses.Application/DependencyInjection.cs
+++ expense-track-backend/src/Modules/Expenses/Expenses.Application/DependencyInjection.cs
@@ -1,6 +1,16 @@
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Expenses.Application;
 
-public class DependencyInjection
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddExpensesApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
 }
