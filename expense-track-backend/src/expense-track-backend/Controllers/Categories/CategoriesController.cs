96343a3f89b18bc8560a49cdf8c40dc1af7083f9
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-21 23:07:00 -0400

    implemntacion de los endpoint de creacion y obtencion de categoria donde se extrae el userId del claim, no se pide por el frontend

---
+++ expense-track-backend/src/expense-track-backend/Controllers/Categories/CategoriesController.cs
@@ -0,0 +1,76 @@
+using System.Security.Claims;
+using Categories.Application.Features.CreateCategory;
+using Categories.Application.Features.GetCategories;
+using expense_track_backend.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace expense_track_backend.Controllers.Categories;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public  CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCategories()
+    {
+        try
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (string.IsNullOrEmpty(userIdString))
+                return Unauthorized("token invalido");
+
+            var userId = Guid.Parse(userIdString);
+
+            var query = new GetCategoriesQuery(userId);
+            var categories = await _mediator.Send(query);
+
+            return Ok(categories);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequest request)
+    {
+        try
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            
+            if (string.IsNullOrEmpty(userIdString))
+                return Unauthorized("Token inválido.");
+
+            var userId = Guid.Parse(userIdString);
+
+            var command = new CreateCategoryCommand(
+                userId,
+                request.Name,
+                request.Icon,
+                request.Color
+            );
+            
+            var categoryId = await _mediator.Send(command);
+
+            return Created(string.Empty, new
+            {
+                Id = categoryId,
+                Message = "Categoria creada correctamente"
+            });
+        } catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+}
