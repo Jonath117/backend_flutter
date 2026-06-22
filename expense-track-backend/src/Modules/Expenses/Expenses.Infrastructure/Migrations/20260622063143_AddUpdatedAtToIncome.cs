07e2a409a1583d9b279e1eefde8510d6f26c7713
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-06-22 02:35:05 -0400

    migraciones

---
+++ expense-track-backend/src/Modules/Expenses/Expenses.Infrastructure/Migrations/20260622063143_AddUpdatedAtToIncome.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expenses.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtToIncome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
